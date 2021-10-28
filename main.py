88d49355b3b351f810e121a64bedf6aec0747026
Zachery <zacherysmith115@gmail.com>
2021-10-28 17:14:14 -0400

    initial setup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import connexion
+from controller import app
 
-
-app = connexion.FlaskApp(__name__, specification_dir='./')
-app.add_api('swagger.yml')
-app.run(port=8080)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8080', debug=True)
