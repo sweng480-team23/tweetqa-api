21b84953489b3fe2cce0b4605e934692dd29687f
Luke Westfall <lwestfall1@gmail.com>
2021-11-14 08:44:41 -0500

    add env directory to .gitignore

---
+++ utils/bert_model_runner.py
@@ -0,0 +1,58 @@
+import string
+from transformers import BertModel, BertTokenizer
+import torch
+
+class BertModelRunner(object):
+    name: string
+    tokenizer: BertTokenizer
+    model: BertModel
+
+    def __init__(self, name: str, tokenizer: tokenizer, model: BertModel) -> None:
+        self.name = name
+        self.tokenizer = tokenizer
+        self.model = model
+        super().__init__()
+
+    def answer_tweet_question(self, tweet, question):
+        #tokenize question and text as a pair
+        input_ids = self.tokenizer.encode(question, tweet)
+
+        #string version of tokenized ids
+        tokens = self.tokenizer.convert_ids_to_tokens(input_ids)
+
+        #segment IDs
+        #first occurence of [SEP] token
+        sep_idx = input_ids.index(self.tokenizer.sep_token_id)
+        #number of tokens in segment A (question)
+        num_seg_a = sep_idx+1
+        #number of tokens in segment B (text)
+        num_seg_b = len(input_ids) - num_seg_a
+
+        #list of 0s and 1s for segment embeddings
+        segment_ids = [1]*num_seg_a + [0]*num_seg_b
+        assert len(segment_ids) == len(input_ids)
+
+        #model output using input_ids and segment_ids
+        # output = bert_model(torch.tensor([input_ids]).to(device), token_type_ids=torch.tensor([segment_ids]).to(device))
+        self.model.eval()
+        output = self.model(input_ids=torch.tensor([input_ids]), attention_mask=torch.tensor([segment_ids]))
+
+        #reconstructing the answer
+        answer_start = torch.argmax(output.start_logits)
+        answer_end = torch.argmax(output.end_logits)
+        print(f"Answer Start: {answer_start}")
+        print(f"Answer End: {answer_end}")
+        if answer_end >= answer_start:
+            answer = tokens[answer_start]
+            for i in range(answer_start+1, answer_end+1):
+                if tokens[i][0:2] == "##":
+                    answer += tokens[i][2:]
+                else:
+                    answer += " " + tokens[i]
+
+        # if answer.startswith("[CLS]"):
+        #     answer = "Unable to find the answer to your question."
+
+        # print("\nPredicted answer:\n{}".format(answer.capitalize()))
+
+        return answer
