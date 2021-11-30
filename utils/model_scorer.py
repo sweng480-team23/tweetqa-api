f790d4b38cd91f29cbc46b8082849a000b1cd370
Edward Lach <edwarddlach@gmail.com>
2021-11-29 21:17:45 -0500

    Created model scoring script for calculating blue, rouge and meteor scores

---
+++ utils/model_scorer.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import string
+import re
+
+from typing import List
+from nltk.translate.bleu_score import sentence_bleu
+from nlgeval.pycocoevalcap.meteor.meteor import Meteor
+from nlgeval.pycocoevalcap.rouge.rouge import Rouge
+
+
+meteor_scorer = Meteor()
+rouge_scorer = Rouge()
+
+
+def read_data(url: str) -> pd.DataFrame:
+    return pd.read_json(url)
+
+
+def generate_gold_file(df: pd.DataFrame) -> List[dict]:
+    data_dict: dict = df.to_dict('records')
+    return [{'qid': datum['qid'], 'Answer': datum['Answer']} for datum in data_dict]
+
+
+def generate_user_file(df: pd.DataFrame) -> List[dict]:
+    # TODO Make predictions using the model runner to generate the user file
+    return
+
+
+def normalize_answer(s):
+    """Lower text and remove punctuation, articles and extra whitespace."""
+
+    def remove_articles(text):
+        return re.sub(r'\b(a|an|the)\b', ' ', text)
+
+    def white_space_fix(text):
+        return ' '.join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
+def ans_score(ans, gold_list):
+    ans = normalize_answer(ans)
+    gold_list = [normalize_answer(ref) for ref in gold_list]
+    bleu = sentence_bleu([_.split() for _ in gold_list], ans.split(), weights=(1, 0, 0, 0))
+    meteor, _ = meteor_scorer.compute_score({0: gold_list}, {0: [ans]})
+    rouge, _ = rouge_scorer.compute_score({0: gold_list}, {0: [ans]})
+    return {'bleu': bleu, 'meteor': meteor, 'rouge': rouge}
+
+
+def evaluate(gold, pred):
+    idx2gold = {item['qid']: item['Answer'] for item in gold}
+    idx2pred = {item['qid']: item['Answer'] for item in pred}
+    idx2scores = {}
+    for id_ in idx2gold.keys():
+        if isinstance(idx2pred[id_], list):
+            pred_ans = idx2pred[id_][0]
+        else:
+            pred_ans = idx2pred[id_]
+        idx2scores[id_] = ans_score(pred_ans, idx2gold[id_])
+    bleus = [item['bleu'] for item in idx2scores.values()]
+    meteors = [item['meteor'] for item in idx2scores.values()]
+    rouges = [item['rouge'] for item in idx2scores.values()]
+    print({'BLEU': np.mean(bleus), 'METEOR': np.mean(meteors), 'ROUGE': np.mean(rouges)})
+
+    output = {}
+    output['result'] = [
+        {'test_split':
+            {
+                'BLEU-1': np.mean(bleus),
+                'METEOR': np.mean(meteors),
+                'ROUGE': np.mean(rouges)
+            }
+        }
+    ]
+
+    return output
+
+
+if __name__ == '__main__':
+    data: pd.DataFrame = read_data('https://raw.githubusercontent.com/sweng480-team23/tweet-qa-data/main/dev.json')
+    gold = generate_gold_file(data)
+    user = generate_user_file(data)
+    output = evaluate(gold, gold)
+    print(output)
