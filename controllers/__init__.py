1eeb785526b797f21e8671fa46d8920fc63cb898
Zachery <zacherysmith115@gmail.com>
2022-01-16 15:50:17 -0500

    typo

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 
 app = connexion.App(__name__, specification_dir='./')
-SECRET_KEY = os.getENV('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 CORS(app.app)
 # App Config
