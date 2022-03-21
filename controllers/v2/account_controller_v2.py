0cd36fb20eab549639c0667e90c29de5f37ff265
sjchin88 <jinqwem@hotmail.com>
2022-03-21 23:53:59 +0800

    Update for V2 pattern

--- controllers/v2/account_controller_v2.py
+++ controllers/v2/account_controller_v2.py
@@ -1,40 +1,39 @@
-
-
-
+from typing import Type
+from dacite import from_dict
 from containers import Container
 from controllers.v2.abstract.abstract_create_read_controller_v2 import AbstractCreateReadControllerV2
-from controllers.v2.abstract.abstract_read_controller_v2 import AbstractReadControllerV2
 from dtos.v2.account_dto_v2 import AccountLoginRequestV2, AccountLoginResponseV2
-from services import account_service
 from services.account_service import AccountService
 from dependency_injector.wiring import inject, Provide
+from services.visitor_service import VisitorService
 
-
-
-class AccountsView(AbstractReadControllerV2):
+class AccountsView(AbstractCreateReadControllerV2):
+    """Controller Class to handle Admin Account Operations"""
 
     @inject
     def __init__(
             self,
-            account_service: AccountService = Provide[Container.account_service],
+            visitor_service: VisitorService = Provide[Container.visitor_service],
+            account_service: AccountService = Provide[Container.account_service],           
     ):
         super().__init__(
             service=account_service,
+            visitor_service=visitor_service,
             response_dto=AccountLoginResponseV2,
+            request_dto=AccountLoginRequestV2,
         )
-        self.service = account_service
 
     def admin_login(self, request:dict):
         """Controller function to perform admin login authentication and return the token"""
 
-        dto_admin = AccountLoginRequestV2(request)
-
-        new_accservice = AccountService()
-        try:
-            login_admin = new_accservice.login(dto_admin.email, dto_admin.password)
-            login_response = AccountLoginResponseV2(login_admin)
-            return login_response, 200
-        
-        except Exception as e:
-            print(e)
-            return None, 404
+        dto: Type[AccountLoginRequestV2] = from_dict(data_class=self.request_dto, data = request)
+        if dto.email is not None:
+            try:
+                login_admin = self.service.login(dto.email, dto.password)
+                login_response = self.response_dto.from_model(login_admin)
+                return login_response, 200
+            except Exception as e:
+                print(e)
+                return None, 404
+        else:
+            return None, 403
--- dtos/v2/account_dto_v2.py
+++ dtos/v2/account_dto_v2.py
@@ -1,28 +1,22 @@
-
-
 from dataclasses import dataclass
 import uuid
-
+from dtos.v2.abstract.abstract_response_v2 import AbstractResponseV2
 from models.account_model import Account
-
 
 @dataclass
 class AccountLoginRequestV2(object):
     email:str
     password:str
 
-    def __init__(self, request: dict) -> None:
-        self.email = request["email"]
-        self.password = request["password"]
-        super().__setattr__('frozen', True)
-
 @dataclass
-class AccountLoginResponseV2(object):
+class AccountLoginResponseV2(AbstractResponseV2):
     email:str
     token:str
     expiresIn:str
-
-    def __init__(self, account:Account) -> None:
-        self.email = account.email
-        self.token = uuid.uuid4()
-        self.expiresIn = '3600'
+    @staticmethod
+    def from_model(model: Account):
+        return AccountLoginResponseV2(
+            email = model.email,
+            token = uuid.uuid4(),
+            expiresIn = '3600'
+        )
