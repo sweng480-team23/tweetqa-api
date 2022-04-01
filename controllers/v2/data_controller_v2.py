2f628a5a803dd7c90927764a4af80393b2a41914
sjchin88 <jinqwem@hotmail.com>
2022-04-01 20:22:07 +0800

    add api for get random tweet

--- controllers/v2/data_controller_v2.py
+++ controllers/v2/data_controller_v2.py
@@ -23,3 +23,13 @@ class DataView(AbstractCreateReadUpdateControllerV2):
             request_dto=DataCreateRequestV2,
             update_request_dto=DataUpdateRequestV2
         )
+    
+    def read_random_data(self):
+        '''Controller function to retrieve a random data'''
+        
+        random_datum = self.service.read_random()
+        if random_datum is not None:
+            response = self.response_dto.from_model(random_datum)
+            return response, 200
+        else:
+            return  None,404
--- services/data_service.py
+++ services/data_service.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from itertools import count
 from services.abstract.create_read_update_service import CreateReadUpdateService
 from services.qa_model_service import QAModelService
+from sqlalchemy.sql import func
 from models.data_model import Data
 from models.qa_model import QAModel
 from typing import List
@@ -10,7 +12,9 @@ import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords', quiet=True)
 import pandas as pd
+import random
 import string
+import timeit
 
 
 class DataService(CreateReadUpdateService):
@@ -45,3 +49,20 @@ class DataService(CreateReadUpdateService):
             for datum in data]
         word_counts: Dict = pd.Series(word_list, dtype=str).value_counts().head(100).to_dict()
         return [{'name': key, 'weight': value} for key, value in word_counts.items()]
+
+    def read_random(self)->Data:
+        # id_list: List[int] = []
+        # all_datas = Data.query.all()
+        # for datum in all_datas:
+        #     id_list.append(datum.id)
+        # #print(id_list)
+        # random_id = random.choice(id_list)
+        # #print(random_id)
+        # random_data = self.read_by_id(random_id)
+        # #print(random_data)
+        starttime = timeit.default_timer()
+        random_data = Data.query.order_by(func.random()).first()
+        #print(random_data)
+        print(timeit.default_timer()-starttime)
+        return random_data
+
