d4b9256ba94cc9b0bf1295cd9707c3e9cb034579
Edward Lach <edwarddlach@gmail.com>
2022-04-03 16:34:43 -0400

    Added dtos, model, service and controller logic to initiate a ML pipeline training given a provided training request

---
+++ dtos/v2/training_dto_v2.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from dtos.v2.abstract.abstract_dto_v2 import AbstractRequestV2
+from dtos.v2.abstract.abstract_dto_v2 import AbstractResponseV2
+from dtos.v2.account_dto_v2 import AccountLoginResponseV2
+from models.training_model import Training
+from datetime import datetime
+
+
+@dataclass
+class TrainingCreateRequestV2(AbstractRequestV2):
+    admin: AccountLoginResponseV2
+    epochs: int
+    learningRate: str
+    batchSize: int
+    baseModel: str
+    lastXLabels: int
+    includeUserLabels: bool
+    pipelineHost: str
+
+    def to_model(self) -> Training:
+        return Training(
+            created=datetime.now(),
+            epochs=self.epochs,
+            learningRate=self.learningRate,
+            batchSize=self.batchSize,
+            baseModel=self.baseModel,
+            lastXLabels=self.lastXLabels,
+            includeUserLabels=self.includeUserLabels,
+            pipelineHost=self.pipelineHost
+        )
+
+
+@dataclass
+class NewTrainingResponseV2(object):
+    message: str
+
+
+@dataclass
+class TrainingResponseV2(AbstractResponseV2):
+    id: int
+    admin: AccountLoginResponseV2
+    epochs: int
+    learningRate: str
+    batchSize: int
+    baseModel: str
+    lastXLabels: int
+    includeUserLabels: bool
+    pipelineHost: str
+
+    @staticmethod
+    def from_model(model: Training):
+        return TrainingResponseV2(
+            id=model.id,
+            admin=AccountLoginResponseV2.from_model(model.admin),
+            epochs=model.epochs,
+            learningRate=model.learningRate,
+            batchSize=model.batchSize,
+            baseModel=model.baseModel,
+            lastXLabels=model.lastXLabels,
+            includeUserLabels=model.includeUserLabels,
+            pipelineHost=model.pipelineHost
+        )
---
+++ models/training_model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import String, DateTime, Integer, Boolean
+from controllers import db
+from models import IdentifiableEntity
+
+
+class Training(IdentifiableEntity):
+    __tablename__ = 'training'
+
+    created = db.Column(DateTime, nullable=False)
+    epochs = db.Column(Integer, nullable=False)
+    learningRate = db.Column(String(10), nullable=False)
+    batchSize = db.Column(Integer, nullable=False)
+    baseModel = db.Column(String(100), nullable=False)
+    lastXLabels = db.Column(Integer, nullable=False)
+    includeUserLabels = db.Column(Boolean, nullable=False)
+    pipelineHost = db.Column(String(100), nullable=True)
+    admin_id = db.Column(Integer, db.ForeignKey('account.id'), nullable=False)
+    admin = db.relationship('models.account_model.Account', back_populates='trainings')
+
+    def __repr__(self) -> str:
+        return f"Training Id: {self.id}, Created: {self.created}, Base Model: {self.baseModel}"""
