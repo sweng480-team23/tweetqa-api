e610d3e3cf588693164a3d619b54b658127df6e4
Luke Westfall <lwestfall1@gmail.com>
2022-04-12 19:50:19 -0400

    check for test flag in prediction service

--- services/prediction_service.py
+++ services/prediction_service.py
@@ -1,6 +1,7 @@
 from models import Prediction
 from controllers import app
 from .abstract.create_read_update_service import CreateReadUpdateService
+import sys
 import requests
 
 
@@ -13,14 +14,22 @@ class PredictionService(CreateReadUpdateService):
         super().__init__(Prediction)
 
     def create(self, prediction: Prediction) -> Prediction:
+        # todo: find a more elegant solution for mocking out the post request to runner service
+        if 'pytest' not in sys.argv[0]:
+            prediction.prediction = self.get_answer(prediction)
+        else:
+            prediction.prediction = "mock prediction"
+        saved_prediction = super().create(prediction)
+        return saved_prediction
+
+
+    def get_answer(self, prediction: Prediction):
         runner_address = app.app.config['RUNNER_ADDRESS']
         dict_out = {"tweet": prediction.datum.tweet, "question": prediction.datum.question}
         res = requests.post(runner_address, json=dict_out)
         dict_in = res.json()
+        return dict_in["answer"]
 
-        prediction.prediction = dict_in["answer"]
-        saved_prediction = super().create(prediction)
-        return saved_prediction
 
     def update(self, prediction: Prediction) -> Prediction:
         # TODO: Logic to be added to update the DATA collection
